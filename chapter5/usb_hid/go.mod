371a301dd2639712f2cf4b76b4f64435dc040c7b
hinagithub <matsuno.hinako@gmail.com>
2025-07-10 09:40:18 +0900

    キーボードの方だけ

---
+++ chapter5/usb_hid/go.mod
@@ -0,0 +1,3 @@
+module github.com/hinagithub/tinygo-work/chapter5/usb_hid
+
+go 1.24.3
---
+++ chapter5/usb_hid/main.go
@@ -0,0 +1,51 @@
+// USB_HID
+// Universal Serial Bus Human Interface Device
+// 人間のインターフェースデバイス = キーボードやマウス
+
+package main
+
+import (
+	"machine"
+	"machine/usb/hid/keyboard"
+	"time"
+)
+
+func main() {
+
+	button := machine.BUTTON
+	button.Configure(machine.PinConfig{Mode: machine.PinInputPulldown})
+
+	kb := keyboard.New()
+
+	for {
+		// button.Get() ・・・ ボタンを押してればtrue、押していなければfalse
+		if !button.Get() {
+			kb.Write([]byte("echo "))
+
+			// Tigy
+			kb.Down(keyboard.KeyModifierShift) // Shiftキー押下
+			kb.Press(keyboard.KeyT)
+			kb.Up(keyboard.KeyModifierShift) // Shiftキーを離す
+			kb.Press(keyboard.KeyI)
+			kb.Press(keyboard.KeyN)
+			kb.Press(keyboard.KeyY)
+
+			// Go
+			kb.Down(keyboard.KeyModifierShift) // Shiftキー押下
+			kb.Press(keyboard.KeyG)
+			kb.Up(keyboard.KeyModifierShift) // Shiftキーを離す
+			kb.Press(keyboard.KeyO)
+
+			kb.Press(keyboard.KeyReturn)
+			time.Sleep(200 * time.Millisecond)
+
+		}
+	}
+}
+
+// キーボードでTinyGoと入力するだけ
+func writeTinyGo() {
+	kb := keyboard.New()
+	time.Sleep(1 * time.Second)
+	kb.Write([]byte("TynyGo"))
+}
