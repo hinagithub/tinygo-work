7943d760b053d55c5ee7c922cf3cecdfa77dfa3e
hinagithub <matsuno.hinako@gmail.com>
2025-07-13 22:47:53 +0900

    Add

--- chapter5/usb_hid_mouse/main.go
+++ chapter5/usb_hid_mouse/main.go
@@ -1,12 +1,49 @@
 package main
 
 import (
+	"machine"
 	"machine/usb/hid/mouse"
 	"time"
 )
 
 func main() {
+
+	button := machine.BUTTON
+	button.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+
 	m := mouse.New()
-	time.Sleep(1 * time.Second) // パソコンとの接続確率待ち
+
+	for {
+		if button.Get() {
+			m.Press(mouse.Left)
+			for i := 0; i < 100; i++ {
+				m.Move(1, 0)
+				time.Sleep(1 * time.Millisecond)
+			}
+			for i := 0; i < 100; i++ {
+				m.Move(0, 1)
+				time.Sleep(1 * time.Millisecond)
+			}
+			for i := 0; i < 100; i++ {
+				m.Move(-1, 0)
+				time.Sleep(1 * time.Millisecond)
+			}
+			for i := 0; i < 100; i++ {
+				m.Move(0, -1)
+				time.Sleep(1 * time.Millisecond)
+			}
+			for i := 0; i < 100; i++ {
+				m.Move(10, 10)
+				time.Sleep(1 * time.Millisecond)
+			}
+
+		}
+	}
+
+}
+
+func moveMounse() {
+	m := mouse.New()
+	time.Sleep(5 * time.Second) // パソコンとの接続確率待ち
 	m.Move(100, 0)
 }
