aa2f47af5f3afbf4513b896504d5e7f7e87aca79
hinagithub <matsuno.hinako@gmail.com>
2025-07-28 09:33:12 +0900

    Add

--- chapter5/trng/main.go
+++ chapter5/trng/main.go
@@ -1,1 +1,17 @@
+// TRNG = True Random Number Generator
 package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"time"
+)
+
+func main() {
+	var result [4]byte
+	for {
+		rand.Read(result[:])
+		fmt.Printf("% X\n\r", result)
+		time.Sleep(time.Second)
+	}
+}
